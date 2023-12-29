194f33d1501311663a7145f047b87b95c01ee5ca
Echenje <stephen.osiru@spinmobile.co.ke>
2023-12-29 10:12:04 +0300

    query

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,107 @@
 import 'package:flutter/material.dart';
-import 'package:sms_autofill/sms_autofill.dart';
+import 'package:flutter_sms_inbox/flutter_sms_inbox.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _smsCode = '';
+class _MyAppState extends State<MyApp> {
+  final SmsQuery _query = SmsQuery();
+  List<SmsMessage> _messages = [];
 
   @override
   void initState() {
     super.initState();
-    _listenForSms();
   }
 
-  void _listenForSms() async {
-    await SmsAutoFill().listenForCode;
+  bool isMpesaMessage(String? body) {
+    // Customize this function based on the structure of M-Pesa messages
+    return body?.contains('M-PESA') == true && body?.contains('Confirmed') == true;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('SMS Reader'),
+    return MaterialApp(
+      title: 'Flutter SMS Inbox App',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'SMS Code:',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 10),
-            Text(
-              _smsCode,
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Pesaway SMS Retriever'),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: _messages.isNotEmpty
+              ? _MessagesListView(
+                  messages: _messages,
+                )
+              : Center(
+                  child: Text(
+                    'No messages to show.\n Tap refresh button...',
+                    style: Theme.of(context).textTheme.headlineSmall,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            var permission = await Permission.sms.status;
+            if (permission.isGranted) {
+              final messages = await _query.querySms(
+                kinds: [
+                  SmsQueryKind.inbox,
+//                   SmsQueryKind.sent,
+                ],
+                count: 10,
+              );
+
+              debugPrint('sms inbox messages: ${messages.length}');
+
+              // Filter only M-Pesa messages
+              final mpesaMessages = messages.where((message) => isMpesaMessage(message.body)).toList();
+
+              setState(() => _messages = mpesaMessages);
+            } else {
+              await Permission.sms.request();
+            }
+          },
+          child: const Icon(Icons.refresh),
         ),
       ),
+    );
+  }
+}
+
+class _MessagesListView extends StatelessWidget {
+  const _MessagesListView({
+    Key? key,
+    required this.messages,
+  }) : super(key: key);
+
+  final List<SmsMessage> messages;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: messages.length,
+      itemBuilder: (BuildContext context, int i) {
+        var message = messages[i];
+
+        return ListTile(
+          title: Text('${message.sender} [${message.date}]'),
+          subtitle: Text('${message.body}'),
+        );
+      },
     );
   }
 }
