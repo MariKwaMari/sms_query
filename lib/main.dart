f8aec720a276f1473ce24ec5297445a089581bd1
Katunga <jmutua289@gmail.com>
2023-12-29 19:12:02 +0300

    home cards

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter SMS Inbox App',
       theme: ThemeData(
+        useMaterial3: true,
         primarySwatch: Colors.teal,
       ),
       home: const HomeScreen(),
